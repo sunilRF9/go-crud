7380c4ef64d47b956ef0c4dd7d4702c6096c3f47
Sunil Subramanya <sunil1996@gmail.com>
2022-06-24 11:41:19 +0530

    First commit

---
+++ routes/entries.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	""
+)
+
+func AddEntry(c *gin.Context) {
+}
+
+func GetEntries(c *gin.Context) {
+}
+
+func GetEntryById(c *gin.Context) {
+}
+
+func GetEntryByIngredient(c *gin.Context) {
+}
+
+func UpdateEntry(c *gin.Context) {
+}
+
+func UpdateIngredient(c *gin.Context) {
+}
+
+func DeleteEntry(c *gin.Context) {
+}
